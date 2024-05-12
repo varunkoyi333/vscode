e35fe0e66a6668f3654d3324abdcd7cfc177df54
Deepak <udeepu2@gmail.com>
2024-05-12 09:31:03 +0530

    OOPS concepts covered.>!

---
+++ OOPS/src/Driver.java
@@ -0,0 +1,18 @@
+public class Driver implements Demo02 {
+    public static void main(String[] args) {
+       Driver obj=new Driver();
+       
+    }
+
+    @Override
+    public void meth3() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'meth3'");
+    }
+
+    @Override
+    public void meth4() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'meth4'");
+    }
+}
